dc2bc195a599cade35051320ead35d4b1acbe949
runzh <18309862+rogerwwww@users.noreply.github.com>
2025-01-20 14:35:24 -0500

    fix install script

--- install.sh
+++ install.sh
@@ -2,8 +2,12 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+# upgrade gcc & gxx versions
+conda install -y gcc gxx
 
 # install requirements
+pip install pip==24.0
+pip install numpy==1.22.4
 pip install torch==1.9.1+cu111 torchaudio -f https://download.pytorch.org/whl/cu111/torch_stable.html
 # install torchaudio, thus fairseq installation will not install newest torchaudio and torch(would replace torch-1.9.1)
 pip install lmdb
@@ -14,9 +18,9 @@ pip install tensorboardX==2.4.1
 pip install ogb==1.3.2
 pip install rdkit-pypi==2021.9.3
 pip install dgl==0.7.2 -f https://data.dgl.ai/wheels/repo.html
+pip install protobuf==3.20.3
 
-cd fairseq
-# if fairseq submodule has not been checkouted, run:
-# git submodule update --init --recursive
-pip install . --use-feature=in-tree-build
+cd engine
+pip install -e .
 python setup.py build_ext --inplace
+# you may have to add #include <cstdint> in pythonpath/lib/python3.9/site-packages/torch/include/pybind11/attr.h
